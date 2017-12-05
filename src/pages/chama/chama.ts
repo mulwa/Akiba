9cf203b460555c17b3bc0571434029cd5253fdc6
mulwa <mulwatech@gmail.com>
2017-12-05 18:51:11 +0300

    added chama filter

--- src/pages/chama/chama.ts
+++ src/pages/chama/chama.ts
@@ -10,19 +10,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChamaPage {
   private mychamas:any[];
+  private chama:string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.chama = "accounts"; 
+
+    this.initializeChamas();   
+  }
+  
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ChamaPage');
+  }
+  initializeChamas(){
     this.mychamas = [
       { name:"Open Republic",members:"51"},
       { name:"Gdg Juja", members:"100"},
       { name:"Young Proffessionals", members:"30"},
       { name:"Akiba Bora",members:"20" }
     ]
-  }
-  
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ChamaPage');
+  }
+  getChama(event:any){
+    // reset all chamas
+    this.initializeChamas();
+    let val =event.target.value;
+    // make sure that the user types something
+    if(val && val.trim() != ''){
+      this.mychamas = this.mychamas.filter((item)=>{
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+        
   }
 
 }
